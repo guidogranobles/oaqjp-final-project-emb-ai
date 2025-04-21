d4b3127bada2491a2be7cdd33ef6ff3a128428c7
ggranobles <gjnobles@outlook.com>
2025-04-21 11:25:15 -0400

    improve code formatting

--- server.py
+++ server.py
@@ -1,20 +1,21 @@
-from flask import Flask, request, render_template, jsonify, make_response
+from flask import Flask, request, render_template, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
-import json
 
 app = Flask(__name__)
 app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
-app.config["JSON_SORT_KEYS"]              = False
+app.config["JSON_SORT_KEYS"] = False
 
 @app.route("/")
 def index():
+    """Return lading page."""
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    """Return result of emotion analysis."""
     text_to_analyze = request.args["textToAnalyze"]
     result = emotion_detector(text_to_analyze)
-    if result["dominant_emotion"] == None:
-       return  {"message": "Invalid text! Please try again!."}, 400
+    if result["dominant_emotion"] is None:
+        return {"message": "Invalid text! Please try again!."}, 400
 
     return jsonify(result), 200
