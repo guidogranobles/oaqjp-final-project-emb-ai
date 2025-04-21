7a6dcb87cad32668412f536ac0ebbb8739112c29
ggranobles <gjnobles@outlook.com>
2025-04-21 11:54:54 -0400

    Fixed error handling

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,7 +5,11 @@ Headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_sto
 
 
 def emotion_detector(text_to_analyze):
-    if text_to_analyze.strip() == "":
+
+    input_json = {"raw_document": {"text": text_to_analyze}}
+    response =  requests.post(URL, headers=Headers, json=input_json).json()
+
+    if response.status_code == 400:
         return {
                "anger": None, 
                "disgust": None, 
@@ -15,8 +19,6 @@ def emotion_detector(text_to_analyze):
                "dominant_emotion": None
         }
 
-    input_json = {"raw_document": {"text": text_to_analyze}}
-    response =  requests.post(URL, headers=Headers, json=input_json).json()
     emotions = response["emotionPredictions"][0]["emotion"]
 
     score = 0
